a4910b652bc599b337f51e8b6e4cc27fb5744f45
mohittomar902 <77494722+mohittomar902@users.noreply.github.com>
2021-06-10 17:59:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-var action="fa fa-edit add-btn";
+import { useEffect, useState } from 'react';
+var action="fa fa-plus add-btn";
 function App() {
+  const getData = () =>{
+    let listOfTask=localStorage.getItem('task');
+    if(listOfTask){
+      return JSON.parse(localStorage.getItem('task'));
+    }
+    else
+    {
+      return [];
+  }
+ }
+
   const [inputData,setInputData] = useState('');
-  const [items,setItems]=useState([]);
-  var action="fa fa-plus add-btn";
+  const [items,setItems]=useState(getData());
+
+
   const addItems = () => {
     if(!inputData){
 
@@ -15,6 +27,7 @@ function App() {
       }
   ); 
   setInputData("");
+  action="fa fa-plus add-btn";
  }
    
  };
@@ -34,15 +47,19 @@ function App() {
 
    }
  }
-
+ useEffect(()=>{
+   localStorage.setItem('task',JSON.stringify(items))
+ },[items]);
+   
+ 
   return (
      <div className="main-div">
         <div className="container">
             <div className="na">
                  <h1> <span className="na1">To</span><span className="na2">Do</span><span className="na3">List</span></h1>
             </div>
-           <div className="datItem" >
-                <input  type="text" placeholder="Add Task...."
+           <div className="data-div">
+                <input  className="datItem" type="text" placeholder="Add Task...."
                     value={inputData}
                    onChange = {(e) =>setInputData(e.target.value)}
                             />
@@ -59,7 +76,7 @@ function App() {
              
              <h3  id={index}>{ele}</h3> 
              <div className="btns">
-               <i className="fa fa-edit add-btn" onClick={()=>editItem(index,items)}>    </i>
+               <i className="fa fa-edit edit-btn" onClick={()=>editItem(index,items)}>    </i>
              <i className="fa fa-trash-o del-btn" onClick={()=>deletItem(index)}	> </i>
              </div>
             </div>
